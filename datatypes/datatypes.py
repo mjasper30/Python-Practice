4fbba3b84334f5167de2f0171db9b401f31c4ff2
mjasper30 <jasper.macaraeg42@gmail.com>
2023-08-13 19:02:05 +0800

    numbers

---
+++ datatypes/datatypes.py
@@ -0,0 +1,29 @@
+"""
+Python DataTypes
+
+Text Type:	str
+Numeric Types:	int, float, complex
+Sequence Types:	list, tuple, range
+Mapping Type:	dict
+Set Types:	set, frozenset
+Boolean Type:	bool
+Binary Types:	bytes, bytearray, memoryview
+None Type:	NoneType
+"""
+
+#String
+name = "Jasper"
+int = 1
+float = 1.25
+complex = 1j
+list = ["list 1", "list 2", "list 3"]
+tuple = ("tuple 1", "tuple 2", "tuple 3")
+range = range(6)
+dict = {"name" : "John", "age" : 36}
+set = (("set1", "set2", "set3"))
+frozenset = (("frozen1", "frozen2", "frozen3"))
+boolean = True
+bytes = b"Hello"
+byte_array = bytearray(6)
+memoryview = memoryview(bytes(5))
+none = None
