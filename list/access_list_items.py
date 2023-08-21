2286af5c7a9893c04864e68bbdd030ad2ddbfda8
mjasper30 <jasper.macaraeg42@gmail.com>
2023-08-21 20:25:14 +0800

    access list items

---
+++ list/access_list_items.py
@@ -0,0 +1,14 @@
+fruits = ["Orange", "Banana", "Apple", "Apple", 1, True]
+
+print(fruits[0])
+print(fruits[-2])
+# fruits[index:how many items you want]
+print(fruits[0:4])
+print(fruits[:5])
+print(fruits[4:])
+print(fruits[-4:-2])
+
+if "Apple" in fruits:
+    print("Apple is in the list")
+else:
+    print("Apple is not exist in the list")
