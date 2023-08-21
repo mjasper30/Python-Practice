4e0f83b3287410981c163f344c5f267ea1cff547
mjasper30 <jasper.macaraeg42@gmail.com>
2023-08-21 19:53:13 +0800

    yolo projects

--- casting/casting.py
+++ casting/casting.py
@@ -1,16 +1,16 @@
-#int convertion
+# int convertion
 x = int(1)   # x will be 1
-y = int(2.8) # y will be 2
-z = int("3") # z will be 3
+y = int(2.8)  # y will be 2
+z = int("3")  # z will be 3
 
-#float convertion
+# float convertion
 a = float(1)     # x will be 1.0
 b = float(2.8)   # y will be 2.8
 c = float("3")   # z will be 3.0
-d = float("4.2") # w will be 4.2
+d = float("4.2")  # w will be 4.2
 
-#string convertion
-j = str("s1") # x will be 's1'
+# string convertion
+j = str("s1")  # x will be 's1'
 k = str(2)    # y will be '2'
 l = str(3.0)  # z will be '3.0'
 
