7407de2c62d5ce36bed7fd2d4be6efe8b600db42
mjasper30 <jasper.macaraeg42@gmail.com>
2023-08-26 17:57:40 +0800

    problem solving

---
+++ w3schools challenge/problem1.py
@@ -0,0 +1,13 @@
+# Solution 1
+twinkle = """Twinkle, twinkle, little star,
+	How I wonder what you are! 
+		Up above the world so high,   		
+		Like a diamond in the sky. 
+Twinkle, twinkle, little star, 
+	How I wonder what you are"""
+
+# Solution 2
+twinkle2 = "Twinkle, twinkle, little star,\n\tHow I wonder what you are! \n\t\tUp above the world so high,\n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star,\n\t\tHow I wonder what you are"
+
+# print(twinkle)
+print(twinkle2)
---
+++ w3schools challenge/problem2.py
@@ -0,0 +1,8 @@
+"""
+Write a Python program that accepts the user's first and last name and prints them in reverse order with a space between them.
+"""
+
+firstname = input(str("Enter your firstname: "))
+lastname = input(str("Enter your lastname: "))
+
+print("Hello " + firstname + " " + lastname)
