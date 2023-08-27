77545a09b248e6b5b27b52ee9ee61656523e64d6
mjasper30 <jasper.macaraeg42@gmail.com>
2023-08-27 18:00:43 +0800

    add set

---
+++ set/add_set.py
@@ -0,0 +1,9 @@
+fruits = {"orange", "apple", "banana"}
+another_fruits = {"mango", "tomato"}
+list_numbers = [1, 2, 3]
+
+fruits.add("papaya")
+fruits.update(another_fruits)
+fruits.update(list_numbers)
+
+print(fruits)
