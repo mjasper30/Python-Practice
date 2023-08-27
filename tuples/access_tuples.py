75a3bfdbba1a6868cbab63e6dec4c5535dfd20ff
mjasper30 <jasper.macaraeg42@gmail.com>
2023-08-27 09:47:17 +0800

    access tuples

---
+++ tuples/access_tuples.py
@@ -0,0 +1,13 @@
+fruits = ["Orange", "Apple", "Grapes", "Mango", "Cherry"]
+
+print(fruits[1])
+print(fruits[-1])
+print(fruits[1:])
+print(fruits[:-1])
+print(fruits[1:4])
+print(fruits[-4:-1])
+
+if "Orange" in fruits:
+    print("Orange is in the list")
+else:
+    print("Orange is not exist")
