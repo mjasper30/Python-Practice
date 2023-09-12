d40dc552999f639683523560a4f4e5f053172fb8
mjasper30 <jasper.macaraeg42@gmail.com>
2023-09-12 20:27:49 +0800

    wild cards

---
+++ database/wild_cards.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="python_db"
+)
+
+mycursor = mydb.cursor()
+
+sql = "SELECT * FROM customers WHERE address LIKE '%way%'"
+
+mycursor.execute(sql)
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+    print(x)
