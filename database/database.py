47aea241861f203f3af468a21bae4fb0882ae6fd
mjasper30 <jasper.macaraeg42@gmail.com>
2023-09-12 19:55:44 +0800

    database

--- database/database.py
+++ database/database.py
@@ -4,8 +4,16 @@ db = mysql.connector.connect(
     host="localhost",
     user="root",
     passwd="",
+    database="python_db"  # Add this if no error meaning database exist
 )
 
 mycursor = db.cursor()
 
-mycursor.execute("CREATE DATABASE python_db")
+# Create database
+mycursor.execute("CREATE DATABASE mydatabase")
+
+# Show all database exists
+mycursor.execute("SHOW DATABASES")
+
+for x in mycursor:
+    print(x)
