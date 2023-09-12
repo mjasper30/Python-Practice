1b6573d20bb238c47c92e94800ecd74551d779bc
mjasper30 <jasper.macaraeg42@gmail.com>
2023-09-12 20:58:25 +0800

    math

---
+++ math/math.py
@@ -0,0 +1,33 @@
+import math
+
+# Get max and min value
+
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+# Get absolute value
+getAbs = abs(-7.25)
+print(getAbs)
+
+# Get power
+getPow = pow(4, 5)
+print(getPow)
+
+# Get square root
+getSqureRoot = math.sqrt(64)
+print(getSqureRoot)  # returns float value
+
+# Get ceil - round up to its nearest integer
+getCeil = math.ceil(12.1142)
+print(getCeil)  # 13
+
+# Get floor - round down its nearest integer
+getFloor = math.floor(110.5142)
+print(getFloor)
+
+# Get pi
+getPi = math.pi
+print(getPi)  # 3.141592653589793
