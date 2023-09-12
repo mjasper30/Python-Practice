abbb0778e84785d6d01e3949ed314e1512979a21
mjasper30 <jasper.macaraeg42@gmail.com>
2023-09-12 20:41:30 +0800

    update data secured

---
+++ database/update_data_secured.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="python_db"
+)
+
+mycursor = mydb.cursor()
+
+sql = "UPDATE customers SET address = %s WHERE address = %s"
+val = ("Valley 345", "Canyon 123")
+
+mycursor.execute(sql, val)
+
+mydb.commit()
+
+print(mycursor.rowcount, "record(s) affected")
